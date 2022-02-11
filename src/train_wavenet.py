2a060fb2257574677a80a86f60c09b82fa5a7b3d
Daniel Ik <codingeinstein@gmail.com>
2022-02-11 09:09:05 +0100

    added train_wavenet.py

---
+++ src/train_wavenet.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from models.waveae import WaveNet
+
+from data import SpeechDS
+
+from torch.utils.data import DataLoader
+
+from torch.optim.lr_scheduler import StepLR,ReduceLROnPlateau
+
+from tqdm import tqdm
+
+from utils.train_utils import save_loss_plot
+
+torch.manual_seed(42)
+
+TIME_PER_SAMPLE = 1 #1 second
+SAMPLE_RATE = 8_000 #8khz is good enough for human speech
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+train = SpeechDS(train=True,
+                time_per_sample=TIME_PER_SAMPLE,
+                sample_rate=SAMPLE_RATE,
+)
+
+test = SpeechDS(train=False,
+                time_per_sample=TIME_PER_SAMPLE,
+                sample_rate=SAMPLE_RATE,
+)
+
+trainloader = DataLoader(train,32,shuffle=True)
+testloader = DataLoader(test,32,shuffle=True)
+
+net = WaveNet(int(train.sample_rate*train.time_per_sample))
+
+net.to(device)
+
+lossfn = nn.MSELoss()
+
+lr = 1e-2
+
+optimizer = optim.Adam(net.parameters(),lr=lr)
+
+lrscheduler = ReduceLROnPlateau(optimizer,mode='min',patience=2,)
+
+train_loss_over_time = []
+test_loss_over_time = []
+
+
+best_loss = float('inf')
+
+for epoch in tqdm(range(100)):
+
+    train_loss_epoch = []
+    test_loss_epoch = []
+    net.train()
+    for x,y in trainloader:
+
+        x = x.to(device)
+        y = y.to(device)
+
+        optimizer.zero_grad()
+
+        p = net(x)
+        loss = lossfn(p,y)
+                
+        loss.backward()
+
+        optimizer.step()
+
+        train_loss_epoch.append(loss.item())
+
+            
+
+    net.eval()
+    with torch.no_grad():
+        for x,y in testloader:
+
+            x = x.to(device)
+            y = y.to(device)
+
+            p = net(x)
+            loss = lossfn(p,y)
+            test_loss_epoch.append(loss.item())
+
+        
+    train_loss_over_time.append(sum(train_loss_epoch)/len(train_loss_epoch))
+    test_loss_over_time.append(sum(test_loss_epoch)/len(test_loss_epoch))
+
+
+    lrscheduler.step(test_loss_over_time[-1])
+
+    save_loss_plot(train_loss_over_time,
+                    test_loss_over_time,
+                    '../plots/WaveNetLoss'
+            )
+
+
+    if test_loss_over_time[-1] <best_loss:
+        best_loss = test_loss_over_time[-1]
+
+        net.save_model('../models')
