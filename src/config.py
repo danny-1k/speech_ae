3c8b2a5a7206878f8419cb652bf1ec0aa05944ea
Daniel Ik <codingeinstein@gmail.com>
2022-02-12 14:49:11 +0100

    updated config.py

--- src/config.py
+++ src/config.py
@@ -1,18 +1,14 @@
-class AudioConfig:
+class MelSpecConfig:
+    ##### TRAIN
     SAMPLE_RATE = 16_000
     MAX_TIME = 1
     N_FFT = 1024
     HOP_LEN = 512
     N_MELS = 64
 
-class TrainConfig:
+    ###### AUDIO
+
     TRAIN_MU = -13.7342
     TRAIN_STD = 16.1290
 
     TRAIN_PCT = .8
-
-
-
-class Config:
-    train_config = TrainConfig
-    audio_config = AudioConfig
