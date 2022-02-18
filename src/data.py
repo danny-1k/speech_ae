dfa17852dcda5afb44d4a189b1221c4e4549c9ff
Daniel Ik <codingeinstein@gmail.com>
2022-02-19 00:04:33 +0100

    shuffle data

--- src/data.py
+++ src/data.py
@@ -24,12 +24,17 @@ class MelSpec(Dataset):
         if train:
             self.data = [os.path.join('../data/train/mel_spec',f) for f in os.listdir('../data/train/mel_spec')]
 
+            np.random.shuffle(self.data)
+
             # self.transform = tatransforms.Compose([
             #     self._random_gaussian_noise(),
             # ])
 
         else:
             self.data = [os.path.join('../data/test/mel_spec',f) for f in os.listdir('../data/test/mel_spec')]
+            
+            np.random.shuffle(self.data)
+            
             # self.transform = tatransforms.Compose([
             # ])
 
@@ -68,7 +73,7 @@ class MelSpec(Dataset):
 
 
 class MFCC(Dataset):
-    def __init__(self,MU,STD,train=True):
+    def __init__(self,MU,STD,train=True,num_train=None,num_test=None):
         super().__init__()
         self.train = train
         self.mu = MU
@@ -77,8 +82,16 @@ class MFCC(Dataset):
         if train:
             self.data = [os.path.join('../data/train/mfcc',f) for f in os.listdir('../data/train/mfcc')]
 
+            np.random.shuffle(self.data)
+            if num_train:
+                self.data = self.data[:num_train]
+
         else:
             self.data = [os.path.join('../data/test/mfcc',f) for f in os.listdir('../data/test/mfcc')]
+
+            np.random.shuffle(self.data)
+            if num_test:
+                self.data = self.data[:num_test]
 
 
     def _standardize(self,x):
@@ -93,6 +106,50 @@ class MFCC(Dataset):
         x = torch.from_numpy(x)
 
         x = self._standardize(x)
+
+
+        return x, x
+
+    def __len__(self):
+        return len(self.data)
+
+
+class Waveform(Dataset):
+    def __init__(self,MU,STD,train=True,num_train=None,num_test=None):
+        super().__init__()
+        self.train = train
+        self.mu = MU
+        self.std = STD
+
+        if train:
+            self.data = [os.path.join('../data/train/waveform',f) for f in os.listdir('../data/train/waveform')]
+
+            np.random.shuffle(self.data)
+            if num_train:
+                self.data = self.data[:num_train]
+
+        else:
+            self.data = [os.path.join('../data/test/waveform',f) for f in os.listdir('../data/test/waveform')]
+
+            np.random.shuffle(self.data)
+            if num_test:
+                self.data = self.data[:num_test]
+
+
+    def _standardize(self,x):
+        x = (x-self.mu)/self.std
+
+        return x
+
+
+    def __getitem__(self, idx):
+        x = np.load(self.data[idx])
+        
+        x = torch.from_numpy(x)
+
+        x = self._standardize(x)
+
+        x = x.view(1,-1)
 
 
         return x, x
